45f7ed537f0361c1d83c833c8d4b3cda0630f055
Caio Espindola <caioespindola@hotmail.com>
2023-03-07 10:58:45 -0300

    studies

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+  .then(resposta => resposta.json())
+  .then(r => {
+    //Parâmetro do if e else nao precisa especificar se é true
+    if (r.erro) {
+      throw Error('Esse CEP não existe!')
+    } else {
+      console.log(r)
+    }
+  })
+  .catch(erro => console.log(erro))
+  .finally(mensagem => console.log('Processamento concluído'))
 
 console.log(consultaCEP)
